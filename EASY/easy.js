992baca3b613de102c24f9ddab3ccc1b3765ca55
Khayla <K.McCray4873@hotmail.com>
2022-05-11 22:17:38 -0400

    finish pls

--- EASY/easy.js
+++ EASY/easy.js
@@ -1,11 +1,23 @@
 //EASY
 /*Write a function that takes in an array of numbers and outputs the average of all the numbers.*/
-Example 1:
-Input: [1 , 4 , 7]  Output: 4
-Input: [10, 5]  Output: 7.5
-Input: [1.5, 3, 2.5, 1]  Output: 2
 
-function average = (arr){
-    arr = [];
+function calculate(average) {
+    var total = 0;
+    var count = 0;
+    
+    average.forEach(function(item) {
+        total += item;
+        count++;
+    });
 
+    return total / count;
 }
+
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+console.log(calculate(array));
+
+
+
+
+
+
